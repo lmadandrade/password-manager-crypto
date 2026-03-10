7a45bfd49aae4cac833cb1fab809c3acb16b168c
lmadandrade <lucas.matheus.alves14@live.com>
2026-03-10 19:57:34 +0000

    Implemented and tested cryptography functions

--- src/crypto_utils.py
+++ src/crypto_utils.py
@@ -1,24 +1,41 @@
-# crypto_utils.py
+import os #generate secure random values
 
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+# crypto_utils.py
 
 # This file contains all cryptographic operations used in the password manager.
 # It handles key generation, encryption and decryption.
 
 def generate_salt():
     # Create a random salt value used when generating the encryption key 
-    pass
+    return os.urandom(16) # generates cryptographically secure random bytes (16 = 128 bits)
 
 
 def derive_key(master_password, salt):
-    # Turn the master password into a secure encription key
-    pass
+    # Turn the master password into a secure encryption key
+    kdf = PBKDF2HMAC( # function that turns the master password into a key 
+        algorithm=hashes.SHA256(), # hashing algorithm
+        length=32, # 32-byte key (suitable for AES-256)
+        salt=salt,
+        iterations=100000, # repeats the process X times
+    )
+    return kdf.derive(master_password.encode())
 
 
 def encrypt_password(password, key):
     # Encrypt the password before saving it to the vault
-    pass
+    aesgcm = AESGCM(key) # creates an AES-GCM object using the key generated from the master password
+    nonce = os.urandom(12) # 12 bytes (standard size fro AES-GCM)
+    ciphertext = aesgcm.encrypt(nonce, password.encode(), None) # performs the encryption
+    return nonce, ciphertext
+
 
 
 def decrypt_password(ciphertext, nonce, key):
-    # Decrypt the stored password then the user wants to view it
-    pass
+    # Decrypt the stored password when the user wants to view it
+    aesgcm = AESGCM(key) 
+    plaintext = aesgcm.decrypt(nonce, ciphertext, None) # decrypts process
+    return plaintext.decode() # coverts bytes back into normal text
---
+++ test_crypto.py
@@ -0,0 +1,29 @@
+from src.crypto_utils import generate_salt, derive_key, encrypt_password, decrypt_password
+
+# Test master password
+master_password = "MyMasterPassword"
+
+# Generate salt
+salt = generate_salt()
+
+# Derive encryption key
+key = derive_key(master_password, salt)
+
+# Password we want to protect
+password = "MySecretPassword123"
+
+# Encrypt the password
+nonce, ciphertext = encrypt_password(password, key)
+
+print("Encrypted password:", ciphertext)
+
+# Decrypt the password
+decrypted = decrypt_password(ciphertext, nonce, key)
+
+print("Decrypted password:", decrypted)
+
+# Check if everything worked
+if decrypted == password:
+    print("Test successful: encryption and decryption work correctly")
+else:
+    print("Test failed")
