a4a713b3b16593fdc8415d632be2037ec37118a2
lmadandrade <lucas.matheus.alves14@live.com>
2026-03-10 18:53:10 +0000

    Added app and storage file skeletons

---
+++ src/storage.py
@@ -0,0 +1,13 @@
+# storage.py
+
+# This file handles reading from and writing to the vault file.
+
+
+def load_vault():
+    # Load the saved vault data from the JSON file
+    pass
+
+
+def save_vault(vault_data):
+    # Save the updated vault data to the JSON file
+    pass
